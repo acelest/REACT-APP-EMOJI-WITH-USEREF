9028b20e962c0a99b03872355e3d2233140fa06e
Aubin Djou <116364607+acelest@users.noreply.github.com>
2024-06-02 21:42:41 +0100

    components

--- src/components/forms/form.jsx
+++ src/components/forms/form.jsx
@@ -1,7 +1,8 @@
 import "./form.scss";
-
+import EmojiPickerInput from "../emojiPicker/emojiPickerInput";
 export default function Form() {
   function handleSubmit(e) {
+    //eviter par defaut le comportement du navigateur
     e.preventDefault();
   }
 
@@ -11,6 +12,7 @@ export default function Form() {
       <input type="text" id="name" />
       <label htmlFor="firstname">First Name</label>
       <input type="text" id="firstname" />
+      <EmojiPickerInput />
       <button className="btn">Submit</button>
     </form>
   );
